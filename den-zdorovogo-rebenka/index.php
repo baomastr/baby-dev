e91c07f408214c9a4c15ccd939296f87f939fa8c
Baomastr <smashsux@gmail.com>
2016-10-25 17:53:16 +0300

    den zdorovogo rebenka fixed actions started

--- den-zdorovogo-rebenka/index.php
+++ den-zdorovogo-rebenka/index.php
@@ -1,9 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("");
-?>
-
-    <div class="row padding__row between section__article section__content-items">
+?>    <div class="row padding__row between section__article section__content-items">
         <div class="col__60 col_margin">
             <h2 class="section__article-title">Дни здорового ребенка</h2><span class="section__content-a">Бесплатный прием и назначение лечения</span>
             <p class="text">Открытая клиника приглашает на “Дни здорового ребенка!"<br>Наши доктора примут маленьких пациентов, дадут рекомендации родителям,
@@ -19,9 +17,9 @@ $APPLICATION->SetTitle("");
     <div class="row center-c">
         <h2 class="section__content-h2 section__content-h2_margin">Наши врачи:</h2>
     </div>
-    <div class="row between wrap">
+    <div class="row between padding__row wrap docslide">
         <div class="bx-slider-7">
-            <div class="slide row padding__row between center-c">
+            <div class="slide row between center-c">
                 <div class="section__content-item col__31">
                     <div class="section__content-img"><img src="/images/test/6.png" alt=""></div>
                     <div class="section__content-name"><span class="section__content-title">Антоненко Татьяна Викторовна</span><br>
@@ -59,40 +57,69 @@ $APPLICATION->SetTitle("");
                     <div class="section__content-btn"><a href="" class="button button_blue upper jsOrdDoctor">бесплатный приём</a></div>
                 </div>
             </div>
+            <div class="slide row between center-c">
+                <div class="section__content-item col__31">
+                    <div class="section__content-img"><img src="/images/test/6.png" alt=""></div>
+                    <div class="section__content-name"><span class="section__content-title">Антоненко Татьяна Викторовна</span><br>
+                        <p class="mt"><b>Массажист,</b>&nbsp;
+                            Профессионально владеет множеством массажных методик (более 8-и), Стаж работы - 10 лет
+                        </p>
+                        <p><b>Центр:</b>&nbsp;
+                            Пресненский
+                        </p>
+                    </div>
+                    <div class="section__content-btn"><a href="" class="button button_blue upper jsOrdDoctor">бесплатный приём</a></div>
+                </div>
+                <div class="section__content-item col__31">
+                    <div class="section__content-img"><img src="/images/test/6.png" alt=""></div>
+                    <div class="section__content-name"><span class="section__content-title">Антоненко Татьяна Викторовна</span><br>
+                        <p class="mt"><b>Массажист,</b>&nbsp;
+                            Профессионально владеет множеством массажных методик (более 8-и), Стаж работы - 10 лет
+                        </p>
+                        <p><b>Центр:</b>&nbsp;
+                            Пресненский
+                        </p>
+                    </div>
+                    <div class="section__content-btn"><a href="" class="button button_blue upper jsOrdDoctor">бесплатный приём</a></div>
+                </div>
+            </div>
         </div>
     </div>
-    <!--.row.center-c-->
-    <!--    a(href="").button.button_blue.button_grand бесплатный приём-->
     <div class="row center-c">
         <h2 class="section__content-h2">Прием доступен только по предварительной записи</h2>
     </div>
     <div class="row padding__row">
         <p class="text"><span class="section__article-blue">Посещение включает в себя</span> первичный прием специалиста и получение рекомендации по лечению</p>
     </div>
-    <div class="row padding__row">
-        <p class="text">Для маленьких пациентов доступно более<span class="section__article-blue"> 20 врачебных специальностей:</span></p>
+    <div class="row">
+        <p class="text">Для маленьких пациентов доступно более <span class="section__article-blue"> 20 врачебных специальностей:</span></p>
     </div>
     <div class="row responsive-column">
         <ul class="section__content-ul col__50">
-            <li class="section__content-li"><a href="" class="section__content-link">Педиатр</a></li>
-            <li class="section__content-li"><a href="" class="section__content-link">Логопед</a></li>
-            <li class="section__content-li"><a href="" class="section__content-link">Детский ортопед-травматолог</a></li>
-            <li class="section__content-li"><a href="" class="section__content-link">Детский хирург</a></li>
+            <li class="section__content-li"><span class="section__content-link">Педиатр</span></li>
+            <li class="section__content-li"><span class="section__content-link">Логопед</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский ортопед-травматолог</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский хирург</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский ЛОР</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский невролог</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский дерматолог</span></li>
         </ul>
         <ul class="section__content-ul col__50">
-            <li class="section__content-li"><a href="" class="section__content-link">Детский ортопед-травматолог</a></li>
-            <li class="section__content-li"><a href="" class="section__content-link">Детский хирург</a></li>
-            <li class="section__content-li"><a href="" class="section__content-link">Педиатр</a></li>
-            <li class="section__content-li"><a href="" class="section__content-link">Логопед</a></li>
+            <li class="section__content-li"><span class="section__content-link">Детский эндокринолог</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский рефлексотерапевт</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский мануальный терапевт</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский физиотерапевт</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский врач ЛФК</span></li>
+            <li class="section__content-li"><span class="section__content-link">Детский гинеколог</span></li>
         </ul>
     </div>
-    <div class="row padding__row">
+    <div class="row">
         <p class="text"><span class="section__article-blue">Бесплатно посетить клинику</span> можно 5 и 6 ноября с 9:00 до 19:00 по адресу: г. Москва, улица 1905 года, д. 7с, стр. 1</p>
     </div>
-    <div class="row">
+    <div class="row padding__row">
         <h2 class="section__content-h2">Получить консультацию, записаться на прием или задать вопрос специалисту</h2>
     </div>
-    <div class="row">
+    <div class="padding__row row">
         <div class="form-spec col__100">
             <form action="/algo/addquest.php" class="addquest formAjax">
                 <div class="row between">
@@ -109,27 +136,31 @@ $APPLICATION->SetTitle("");
             </form>
         </div>
     </div>
-    <div class="row padding__row between">
+    <div class="padding__row row">
+        <h3 class="blue"><b>Запись по телефонам: +7 (777) 999-33-11  /  +7 (777) 999-33-11</b></h3>
+    </div>
+    <br>
+    <br>
+    <div class="row padding__row between hidden-xs">
         <div class="col__32">
-            <div class="img-wrapper"><img src="/images/test/healthybaby.png"></div>
+            <div class="img-wrapper"><img src="/images/test/open1.jpg" class="rounded"></div>
         </div>
         <div class="col__32">
-            <div class="img-wrapper"><img src="/images/test/healthybaby.png"></div>
+            <div class="img-wrapper"><img src="/images/test/open2.jpg" class="rounded"></div>
         </div>
         <div class="col__32">
-            <div class="img-wrapper"><img src="/images/test/healthybaby.png"></div>
+            <div class="img-wrapper"><img src="/images/test/open3.jpg" class="rounded"></div>
         </div>
     </div>
-    <div class="row padding__row between">
+    <div class="row padding__row between hidden-xs">
         <div class="col__32">
-            <div class="img-wrapper"><img src="/images/test/healthybaby.png"></div>
+            <div class="img-wrapper"><img src="/images/test/open4.jpg" class="rounded"></div>
         </div>
         <div class="col__32">
-            <div class="img-wrapper"><img src="/images/test/healthybaby.png"></div>
+            <div class="img-wrapper"><img src="/images/test/open5.jpg" class="rounded"></div>
         </div>
         <div class="col__32">
-            <div class="img-wrapper"><img src="/images/test/healthybaby.png"></div>
+            <div class="img-wrapper"><img src="/images/test/open6.jpg" class="rounded"></div>
         </div>
     </div>
-    
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>

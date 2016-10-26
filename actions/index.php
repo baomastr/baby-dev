f3d08a6b38d91319df73f2cf10aabdc30542db1c
Baomastr <smashsux@gmail.com>
2016-10-26 19:35:10 +0300

    2016-10-26 actions and 404 initialised

---
+++ actions/index.php
@@ -0,0 +1,150 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Акции");
+?>
+    <div class="section__content-main">
+        <h1>Позаботьтесь об осанке своего ребенка сейчас</h1>
+        <div class="row padding__row">
+            <h2>Избавьте от болей и отклонений в будущем</h2>
+        </div>
+        <div class="row padding__row">
+            <p class="text">Избавьте от болей и отклонений в будущем<br>До 70% детей школьного возраста страдают нарушением осанки. Если обратиться к врачу вовремя – все они обратимы. В противном случае ребенка ожидают тяжелые последствия – сколиоз, остеохондроз и межпозвоночные грыжи.</p>
+        </div>
+        <div class="row padding__row">
+            <p>Увидеть тревожные симптомы легко:</p>
+        </div>
+        <div class="row padding__row between">
+            <ul class="section__content-list col__50">
+                <li class="section__content-li">Сутуловатость фигуры</li>
+                <li class="section__content-li">Голова наклонена вперед или в сторону</li>
+                <li class="section__content-li">Выступающие лопатки</li>
+            </ul>
+            <ul class="section__content-list col__50">
+                <li class="section__content-li">Сутуловатость фигуры</li>
+                <li class="section__content-li">Голова наклонена вперед или в сторону</li>
+            </ul>
+        </div>
+        <div class="row padding__row">
+            <p class="text">Мы предлагаем быстрый и комфортный для Вас и ребенка способ объективной диагностики состояния осанки за 1 визит</p>
+        </div>
+        <div class="row">
+            <div class="img-wrapper"><img src="images/test/kids.jpg"></div>
+        </div>
+        <div class="row padding__row ta-center mb-20">
+            <h3 class="red"> Мы вовремя выявим проблемы с осанкой и, при необходимости, восстановим ровную спину. для этого мы предлагаем комплекс диагностики, который поможет за несколько часов проверить полное состояние позвоночника вашего ребенка</h3>
+        </div>
+        <div class="padding-row">
+            <div class="table actions">
+                <div class="table__header">
+                    <div class="content__row">
+                        <div class="content__cell">Услуга</div>
+                        <div class="content__cell col__20">Цена, руб.</div>
+                    </div>
+                </div>
+                <div class="table__body">
+                    <div class="content fadeIn animated open">
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия, препарат "Алюсталь" (базовый курс)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия, препарат "Фосталь" (базовый курс)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия, препарат "Алюсталь", поддерживающий курс (1 инъекция)</div>
+                            <div class="content__cell col__20"><b>4 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия, препарат "Алюсталь", начальный курс (1 инъекция)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия, препарат "Фосталь", поддерживающий курс (1 инъекция)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия, препарат "Фосталь", начальный курс (1 инъекция)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Аллергенспецифическая иммунотерапия с 1-м аллергеном (1 процедура)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Скарификационный тест на аллергены (9 аллергенов)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Скарификационный тест на аллергены (18)</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell">Внутрикожный тест с неинфекционным аллергеном</div>
+                            <div class="content__cell col__20"><b>24 000</b></div>
+                        </div>
+                        <div class="content__row">
+                            <div class="content__cell red">Холодовой тест (Дункан-тест)</div>
+                            <div class="content__cell col__20">
+                                <strike>24 000</strike>&nbsp;&nbsp;<br><b class="red">5921</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="padding__row ta-center">
+            <h4>Что вы получите</h4>
+        </div>
+        <div class="section__content-row">
+            <div class="row padding__row between ta-center">
+                <div class="col__25">
+                    <div class="img-wrapper"><img src="images/test/spine.png"></div>
+                    <p>Полную картину состояния позвоночника</p>
+                </div>
+                <div class="col__25">
+                    <div class="img-wrapper"><img src="images/test/foot.png"></div>
+                    <p>Полную картину состояния позвоночника</p>
+                </div>
+                <div class="col__25">
+                    <div class="img-wrapper"><img src="images/test/dance.png"></div>
+                    <p>Полную картину состояния позвоночника</p>
+                </div>
+            </div>
+        </div>
+        <div class="section__content-row">
+            <div class="row ta-center mb-20">
+                <h3 class="red"> Мы вовремя выявим проблемы с осанкой и, при необходимости, восстановим ровную спину. для этого мы предлагаем комплекс диагностики, который поможет за несколько часов проверить полное состояние позвоночника вашего ребенка</h3>
+            </div>
+        </div>
+        <div class="row padding__row between ta-center">
+            <div class="col__25">
+                <div class="img-wrapper"><img src="images/test/resume.png"></div>
+                <p>Полную картину состояния позвоночника</p>
+            </div>
+            <div class="col__25">
+                <div class="img-wrapper"><img src="images/test/charity.png"></div>
+                <p>Полную картину состояния позвоночника</p>
+            </div>
+            <div class="col__25">
+                <div class="img-wrapper"><img src="images/test/doctor.png"></div>
+                <p>Полную картину состояния позвоночника</p>
+            </div>
+        </div>
+        <h2 class="padding__row"><b>Заявка на диагностику ребенка</b></h2>
+        <h2 class="padding__row"><b>Преимущества акции:</b></h2>
+        <ul class="section__content-ul padding__row col__100">
+            <li class="section__content-li">lorem</li>
+            <li class="section__content-li">lorem</li>
+            <li class="section__content-li">lorem</li>
+            <li class="section__content-li">lorem</li>
+        </ul>
+        <h3 class="padding__row"><b>Преимущества акции:</b></h3>
+        <ul class="section__content-ul padding__row col__100">
+            <li class="section__content-li">lorem</li>
+            <li class="section__content-li">lorem</li>
+            <li class="section__content-li">lorem</li>
+            <li class="section__content-li">lorem</li>
+        </ul>
+    </div>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
